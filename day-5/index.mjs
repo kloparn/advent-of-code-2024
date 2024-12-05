06306c2f68cf526c8ea42eb514e55891ee483634
Adam hakansson <adam97.live.com@live.com>
2024-12-05 19:50:41 +0100

    Update day 5.
     - Refactor rule algorithm to keep the rules intact.
    
     - Use sliding window approach to make it easy to find the greatest rule

--- day-5/index.mjs
+++ day-5/index.mjs
@@ -1,16 +1,12 @@
 import fs from "fs";
 
 // Setup the rules
-const [rules, pages] = fs.readFileSync("data", "utf8").trim().split(/\n\n/).reduce(((acc, curr) => {
+const [rules, pages] = fs.readFileSync("data", "utf8").trim().split("\r\n\r\n").reduce(((acc, curr) => {  
   if (curr.includes("|")) {
-    const pages = curr.split(/\n/)
+    const pages = curr.split(/\r\n/)
 
     for (const page of pages) {
-      const [page1, page2] = page.split("|");
-        acc[0][page1] = {
-          ...acc[0][page1],
-          [page2]: true,
-        };
+      acc[0][page] = true;
     }
     return acc;
   } else {
@@ -28,18 +24,13 @@ const invalidPages = [];
 outer: for (const page of pages) {
   const pageNumbers = page.split(",").map(Number);
 
-  for (let i = pageNumbers.length - 1; i >= 0; i--) {
-    let page = pageNumbers[i];
+  for (let i = 0; i < pageNumbers.length - 1; i++) {
+    const left = pageNumbers[i];
+    const right = pageNumbers[i + 1];
 
-    for (let j = i + 1; j < pageNumbers.length; j++) {
-      // check the order of the pages that they match the rules.
-      const underPage = pageNumbers[j];
-
-      if (!rules[page]?.[underPage]) {
-        invalidPages.push(pageNumbers);
-        continue outer;
-      }
-      page = underPage;
+    if (!rules[`${left}|${right}`]) {
+      invalidPages.push(pageNumbers);
+      continue outer;
     }
   }
 
@@ -54,59 +45,40 @@ console.log("Part 1:", validPages.reduce((acc, curr) => {
 
 // part 2: order invalid pages by the rules then count the middle sections like before.
 
-function shuffle(array) {
-  let currentIndex = array.length;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-
-    // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-}
-
 const correctlyShuffled = [];
 
-let index = 0;
-outer: while(invalidPages.length) {
-  for (const page of invalidPages) {
-    shuffle(page);
-  }
+while(invalidPages.length) {
+  const page = invalidPages[0];
 
-  index++;
+  // Using sliding window that has the whole page length,
+  // we can check the orders of all the elements.
 
-  for (const pageNumbers of invalidPages) {
+  const newPage = [];
 
-    for (let i = pageNumbers.length - 1; i >= 0; i--) {
-      let page = pageNumbers[i];
+  let left = 0;
+  let right = page.length - 1;
+  while(page.length) {
+    const leftValue = page[left];
+    const rightValue = page[right];
 
-      // add a variable to make sure that we don't get stuck in loops.
-      const checked = {};
-
-      for (let j = i + 1; j < pageNumbers.length; j++) {
-        // check the order of the pages that they match the rules.
-        const underPage = pageNumbers[j];
-
-        if (!rules[page]?.[underPage] || checked[underPage]) {
-          if (checked[underPage]) {
-            console.log("Stuck in loop")
-          }
-          continue outer;
-        }
-
-        checked[underPage] = true;
-        page = underPage;
-      }
+    if (rules[`${leftValue}|${rightValue}`]) {
+      right--;
+    } else if (rules[`${rightValue}|${leftValue}`]) {
+      left++;
     }
-    // console.log(pageNumbers)
-    console.log("Shuffled", index, "times")
-    correctlyShuffled.push(invalidPages.shift());
+
+    if (left === right) {
+      newPage.push(page[left]);
+      // splice out the pageNumber that had the correct order.
+      page.splice(left, 1);
+      left = 0;
+      right = page.length - 1;
+    }
   }
+
+  // page has now been arranged correctly.
+  correctlyShuffled.push(newPage);
+  invalidPages.shift();
 }
 
 console.log("Part 2:", correctlyShuffled.reduce((acc, curr) => {
