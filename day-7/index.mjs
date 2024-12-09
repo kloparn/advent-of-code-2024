2a76318b7834fb8f93be7d4ed0970ffcbbd889bb
Adam Håkansson <adam.hakansson@bonniernews.se>
2024-12-09 13:47:10 +0100

    Cleanup day-7 for simple reading

--- day-7/index.mjs
+++ day-7/index.mjs
@@ -11,69 +11,45 @@ const equations = data.map((line) => {
   }
 });
 
+const solve = (equations, part2) =>{
+  for (const { answer, equation } of equations) {
+    if (equation.length === 1 && equation[0] === answer) {
+      validAnswers.push(answer);
+      continue;
+    }
+
+    const queue = [{ equation, index: 0, result: 0 }];
+
+    while (queue.length) {
+      const { equation, index, result } = queue.shift();
+
+      if (index > equation.length || result > answer) continue;
+
+      if (index === equation.length) {
+        if (result === answer) {
+          validAnswers.push(answer);
+          break;
+        }
+        continue;
+      }
+
+      const value = equation[index];
+
+      queue.push({ equation, index: index + 1, result: result + value });
+      queue.push({ equation, index: index + 1, result: result * value });
+
+      if (part2) queue.push({ equation, index: index + 1, result: Number(String(result) + String(value)) });
+    }
+  }
+}
+
 const validAnswers = [];
 
-for (const { answer, equation } of equations) {
-  if (equation.length === 1 && equation[0] === answer) {
-    validAnswers.push(answer);
-    continue;
-  }
+solve(equations, false);
 
-  const queue = [{ equation, index: 0, result: 0 }];
-
-  while (queue.length) {
-    const { equation, index, result } = queue.shift();
-
-    if (index === equation.length) {
-      if (result === answer) {
-        validAnswers.push(answer);
-        break;
-      }
-      continue;
-    }
-
-    const value = equation[index];
-
-    queue.push({ equation, index: index + 1, result: result + value });
-    queue.push({ equation, index: index + 1, result: result * value });
-  }
-}
 console.log("Part 1:", validAnswers.reduce((acc, num) => acc + num, 0));
 
-const validAnswersPart2 = [];
+validAnswers.length = 0;
+solve(equations, true);
 
-for (const { answer, equation } of equations) {
-  if (equation.length === 1 && equation[0] === answer) {
-    validAnswers.push(answer);
-    continue;
-  }
-
-  const queue = [{ equation, index: 0, result: 0 }];
-
-  while (queue.length) {
-    const { equation, index, result } = queue.shift();
-
-    if (index > equation.length || result > answer) continue;
-
-    if (index === equation.length) {
-      if (result === answer) {
-        validAnswersPart2.push(answer);
-        break;
-      }
-      continue;
-    }
-
-    const value = equation[index];
-
-    queue.push({ equation, index: index + 1, result: result + value });
-    queue.push({ equation, index: index + 1, result: result * value });
-    queue.push({ equation, index: index + 1, result: Number(String(result) + String(value)) });
-  }
-}
-
-
-
-console.log("Part 2:", validAnswersPart2.reduce((acc, num) => acc + num, 0));
-
-
-
+console.log("Part 2:", validAnswers.reduce((acc, num) => acc + num, 0));
